e5e4b826c1a94952782589120fbefa79931a185c
Kole565 <rjkz565rjkz565@gmail.com>
2022-02-12 22:56:31 +0300

    Add scene functionality - exit functions

--- binary/template/scene.py
+++ binary/template/scene.py
@@ -1,9 +1,11 @@
+import sys
+
 import pygame
 
 
 class Scene:
 
-    """Container for environment, objects and gui on screen"""
+    """Container for updateable objects on screen"""
     
     def __init__(self, screen=None, environment=[], objects=[], gui=[]):
         self.screen = screen
@@ -13,29 +15,39 @@ class Scene:
         self.gui = gui
 
     def update(self):
+        events = pygame.event.get()
+        self.exit_check(events)
+        
         self.update_environment()
         self.update_objects()
         self.update_gui()
 
         self.update_screen()
-        print("Log: update")
+    
+    def exit_check(self, events):
+        """Exit game by input"""
+        for event in events:
+            if (event.type == pygame.QUIT or
+               (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE)):
+                self.exit()
+    
+    def exit(self):
+        pygame.quit()
+        sys.exit()
     
     def update_environment(self):
         self.update_list(self.environment)
-        print("Log: update_environment")
     
     def update_objects(self):
         self.update_list(self.objects)
-        print("Log: update_objects")
     
     def update_gui(self):
         self.update_list(self.gui)
-        print("Log: update_gui")
 
     def update_screen(self):
         pygame.display.update()
-        print("Log: update_screen")
     
     def update_list(self, list):
         for updateable in list:
-            updateable.update()    
+            updateable.update()
+            
