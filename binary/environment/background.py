186f6070df95ca1a35b8d05557dc9e959f79caf1
Kole565 <rjkz565rjkz565@gmail.com>
2022-02-12 22:58:18 +0300

    Move rendering functionality from Background to Renderable

--- binary/environment/background.py
+++ binary/environment/background.py
@@ -4,24 +4,25 @@ import sys
 PROJECT_ROOT = os.path.join(os.path.dirname(__file__), "..")
 sys.path.append(PROJECT_ROOT)
 
-from pygame import image
-import pygame
+from binary.interfaces.renderable import Renderable
 
 
-class Background():
+class Background(Renderable):
 
     """Represent background image"""
 
-    def __init__(self, path, screen=None):
-        self.surface = image.load(path)
+    texture_path = "./resource/image/static/test_back.png"
+
+    def __init__(self, screen=None, texture_path=None):
         self.screen = screen
+        if texture_path:
+            self.texture_path = texture_path
 
         self.coords = (0, 0)
+        self.size = (screen.get_size())
+
+        Renderable.__init__(self, screen)
     
     def update(self):
         self.render()
-    
-    def render(self):
-        self.screen.blit(self.surface, self.coords)
-        print("Log: Background render")
         
