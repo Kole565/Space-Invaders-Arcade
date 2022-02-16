71713dfda7dadf0b8997170a601c64185ed17e93
Kole565 <rjkz565rjkz565@gmail.com>
2022-02-16 17:27:23 +0300

    Add attacker ship class

--- binary/objects/ship.py
+++ binary/objects/ship.py
@@ -8,18 +8,32 @@ class Ship(MoveableByInput, Renderable):
     texture_path = "./resource/image/dynamic/ship/ship.png"
     size = (50, 50)
 
-    vertical_speed = 2
-    horizontal_speed = 2
+    vertical_speed = 1
+    horizontal_speed = 1
 
-    def __init__(self, spawn_pos=(0, 0), screen=None):
+    def __init__(self, screen, spawn_pos=(0, 0)):
         MoveableByInput.__init__(self, spawn_pos)
         Renderable.__init__(self, screen)
-
-        self.gun = BulletGun(screen, self)
 
     def update(self):
         self.move()
         self.render()
+
+
+class Attacker(Ship):
+    
+    size = (30, 30)
+
+    vertical_speed = 2
+    horizontal_speed = 2
+
+    def __init__(self, screen, spawn_pos=(0, 0)):
+        super().__init__(screen, spawn_pos)
+
+        self.gun = BulletGun(screen, self)
+
+    def update(self):
+        super().update()
 
         self.gun.update()
         
