d1dd17a8ca03986c7988f6fbcba912259dde4ef2
Kole565 <rjkz565rjkz565@gmail.com>
2022-02-22 15:27:26 +0300

    Gun rendering implementation

--- binary/objects/gun.py
+++ binary/objects/gun.py
@@ -7,25 +7,28 @@ import pygame
 from time import time
 
 
-class Gun(Moveable):
+class Gun(Moveable, Renderable):
 
     def __init__(self, screen, parent):
         self.screen = screen
         self.parent = parent
 
         Moveable.__init__(self)
+        Renderable.__init__(self, screen)
 
         self.last_shoot = time()
         self.set_mount_offset()
 
     def set_mount_offset(self):
         offset = [0, 0]
-        offset[0] = self.parent.size[0] / 2
+        offset[0] = self.parent.size[0]/2 - self.size[0]/2
+        offset[1] = -self.size[0]/4
 
         self.mount_offset = offset
 
     def update(self):
         self.moving()
+        self.render()
 
     def moving(self):
         x = self.parent.coords[0] + self.mount_offset[0]
@@ -62,12 +65,16 @@ class UserGun(Gun):
 
 class BulletGun(UserGun):
 
+    texture_path = "./resource/image/dynamic/BulletGun.png"
+    size = (24, 24)
+
     shoot_delay = 0.25
 
     def __init__(self, screen, parent):
         super().__init__(screen, parent)
 
         self.bullets = []
+        self.set_bullet_offset()
     
     def update(self):
         super().update()
@@ -77,11 +84,26 @@ class BulletGun(UserGun):
     def update_bullets(self):
         for bullet in self.bullets:
             bullet.update()
+            if bullet.out_of_borders():
+                self.bullets.remove(bullet)
     
     def shoot(self):
         super().shoot()
 
-        self.bullets.append(Bullet(self.screen, self.coords))
+        self.bullets.append(Bullet(self.screen, self.get_bullet_coords()))
+    
+    def get_bullet_coords(self):
+        x = self.coords[0] + self.bullet_offset[0]
+        y = self.coords[1] + self.bullet_offset[1]
+
+        return (x, y)
+    
+    def set_bullet_offset(self):
+        offset = [0, 0]
+        offset[0] = self.size[0]/2
+        offset[1] = -6
+
+        self.bullet_offset = offset
     
     def collision_check(self, objects):
         for bullet in self.bullets:
